e8cd7857d833ba70fe1288ecf40362523c26d893
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-19 10:54:32 -0400

    feat(Root): add _app & _document pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { darkTheme, lightTheme, customTheme } from '../themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
