c05981d1ad67e47c86db44f00406a3b42c0bf0cd
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-19 18:24:44 -0400

    feat(Root): localStorage vs cookies comparison

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,6 +1,7 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import Cookies from 'js-cookie'
 
 import { Layout } from "../components"
 
@@ -10,9 +11,19 @@ const ThemeChangerPage = () => {
     const [currentTheme, setCurrentTheme] = useState('light')
 
     const onThemeChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
-        console.log(target.value);
-        setCurrentTheme(target.value)
+        const selectedTheme = target.value
+
+        console.log(selectedTheme);
+        setCurrentTheme(selectedTheme)
+
+        localStorage.setItem('theme', selectedTheme) //capacidad hasta 5 mb
+        Cookies.set('theme', selectedTheme)          //capacidad hasta 4kb
     }
+
+    useEffect(() => {
+        console.log('LocalStorage', localStorage.getItem('theme'));
+        // axios.post('/api/help',{localStorage.getItem('theme')})  //con localStorage se manda manual la info al backend
+    }, []);
 
 
     return (
