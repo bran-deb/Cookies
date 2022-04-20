7c0c435f863426520a860e0cef0857ce7b090af5
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-19 20:07:58 -0400

    feat(Root): genera pagina con el theme - al usar getInitialProps desactiva las optimizaciones estaticas - desactiva getStaticProps - desactiva getStaticPaths

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,43 @@
 import type { AppContext, AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
 import { darkTheme, lightTheme, customTheme } from '../themes';
 
-function MyApp({ Component, pageProps, ...rest }: AppProps) {
 
-  console.log({ rest });
+interface Props extends AppProps {
+  theme: string
+}
+
+
+function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
+
+  // console.log({ theme });
+  const currentTheme: Theme = theme === 'light'
+    ? lightTheme
+    : (theme === 'dark')
+      ? darkTheme
+      : customTheme
 
 
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   )
 }
 
+//NOTE: al usar getInitialProps desactiva staticPaths y staticProps
+// MyApp.getInitialProps = async (appContext: AppContext) => {
 
-MyApp.getInitialProps = async (appContext: AppContext) => {
-
-  const { theme } = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' }
-  console.log('getInitialProps', theme);
-  const validThemes = ['light', 'dark', 'custom']
+//   const { theme } = appContext.ctx.req ? (appContext.ctx.req as any).cookies : { theme: 'light' }
+//   console.log('getInitialProps', theme);
+//   const validThemes = ['light', 'dark', 'custom']
 
 
-  return {
-    theme: validThemes.includes(theme) ? theme : 'dark'
-  }
-}
+//   return {
+//     theme: validThemes.includes(theme) ? theme : 'dark'
+//   }
+// }
 
 
 export default MyApp
