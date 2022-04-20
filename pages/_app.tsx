c9676739a4c1240cfd4eae4209bf349e0d8e84a2
Brandon Padilla <jairo.padilla.deb19@gmail.com>
2022-04-19 20:28:06 -0400

    feat(Root): solucion de problema match front back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
+
 import type { AppContext, AppProps } from 'next/app'
 import { CssBaseline, Theme, ThemeProvider } from '@mui/material'
 import { darkTheme, lightTheme, customTheme } from '../themes';
+import Cookies from 'js-cookie';
 
 
 interface Props extends AppProps {
@@ -11,11 +14,20 @@ interface Props extends AppProps {
 function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
 
   // console.log({ theme });
-  const currentTheme: Theme = theme === 'light'
-    ? lightTheme
-    : (theme === 'dark')
-      ? darkTheme
-      : customTheme
+
+  const [currentTheme, setCurrentTheme] = useState(lightTheme);
+  //NOTE: el servidor no ejecuta codigo que esta dentro de useEffect
+  //revisa la cookie y retorna el theme al primer render
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme') || 'light'
+    const selectedTheme = cookieTheme === 'light'
+      ? lightTheme
+      : (cookieTheme === 'dark')
+        ? darkTheme
+        : customTheme;
+
+    setCurrentTheme(selectedTheme)
+  }, []);
 
 
   return (
